0ba2ed2ee3f1034e709511af0b82c593670f5e9c
Lexi Pugh <110280389+LexiPugh@users.noreply.github.com>
2023-11-30 17:47:32 -0500

    Update and rename calculation.sql to code.sql
    Adding in my Day 1 Code.

---
+++ coding-lang-calculation-file/code.sql
@@ -0,0 +1,14 @@
+SELECT
+  tweet_count_per_user AS tweet_bucket,
+  COUNT(user_id) AS users_num
+FROM (
+  SELECT 
+    user_id,
+    COUNT(tweet_id) AS tweet_count_per_user
+  FROM 
+    tweets
+  WHERE
+    date_part('year', tweet_date) = 2022
+  GROUP BY
+    user_id) AS total_tweets
+GROUP BY tweet_count_per_user

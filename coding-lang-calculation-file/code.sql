f3c83277f0650a82dab8c567648769860d3d6b67
Lexi Pugh <110280389+LexiPugh@users.noreply.github.com>
2024-03-06 22:42:39 -0500

    Add Day 97

--- coding-lang-calculation-file/code.sql
+++ coding-lang-calculation-file/code.sql
@@ -1063,3 +1063,19 @@ GROUP BY
   department
 ORDER BY
   new_salary DESC
+
+
+SELECT
+  d.employee_id,
+  CASE
+    WHEN SUM(d.deal_size) > s.quota THEN 'yes'
+    ELSE 'no'
+  END AS made_quota
+FROM 
+  deals AS d INNER JOIN sales_quotas AS s 
+  ON d.employee_id = s.employee_id
+GROUP BY
+  d.employee_id,
+  s.quota
+ORDER BY
+  d.employee_id

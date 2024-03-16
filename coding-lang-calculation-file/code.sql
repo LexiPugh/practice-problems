814477ec23ce8aa96d9dbefdbe8685ee147946b9
Lexi Pugh <110280389+LexiPugh@users.noreply.github.com>
2024-03-15 20:56:44 -0400

    Add Day 106

--- coding-lang-calculation-file/code.sql
+++ coding-lang-calculation-file/code.sql
@@ -1176,3 +1176,18 @@ GROUP BY
   employee_name
 ORDER BY
   employee_name
+
+
+SELECT 
+  medication,
+  rec_dosage
+FROM 
+  medication_information
+UNION ALL
+SELECT
+  medication_name,
+  recommended_dosage
+FROM
+  med_list
+ORDER BY
+  medication

3dbac4a32eaeb7e0e3f5c213e00e2bb320b50ca3
Lexi Pugh <110280389+LexiPugh@users.noreply.github.com>
2024-04-10 21:33:28 -0400

    Add Day 132

--- coding-lang-calculation-file/code.sql
+++ coding-lang-calculation-file/code.sql
@@ -1558,3 +1558,21 @@ GROUP BY
   isp_name
 ORDER BY
   average_outage_duration DESC
+
+
+SELECT 
+  driver_id,
+  total_rides,
+  CASE
+    WHEN total_rides < 100 THEN 0
+    WHEN (total_rides >= 100 AND total_rides < 500) THEN 100
+    WHEN (total_rides >= 500 AND total_rides < 1000) THEN 100+500
+    WHEN (total_rides >= 1000 AND total_rides < 2000) THEN 100+500+1000
+    ELSE 100+500+1000+((FLOOR(total_rides/1000) - 1) * 1000)
+  END AS bonus
+FROM 
+  lyft_bonuses
+GROUP BY
+  driver_id
+ORDER BY
+  driver_id

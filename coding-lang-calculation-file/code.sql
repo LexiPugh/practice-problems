4d686bfb807bf7ff48d4385638b3084969170bca
Lexi Pugh <110280389+LexiPugh@users.noreply.github.com>
2024-04-22 22:44:21 -0400

    Add Day 144

--- coding-lang-calculation-file/code.sql
+++ coding-lang-calculation-file/code.sql
@@ -1810,3 +1810,11 @@ FROM (
     transactions ) AS row_num_table
 WHERE
   row_num = 3
+
+
+SELECT 
+  user_id,
+  tweet_date,
+  ROUND(AVG(tweet_count) OVER(PARTITION BY user_id ORDER BY tweet_date ASC ROWS BETWEEN 2 PRECEDING AND CURRENT ROW), 2) AS rolling_average
+FROM 
+  tweets

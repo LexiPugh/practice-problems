7ae91dd09b1c58f94f8aa00b291bce55a69831b9
Lexi Pugh <110280389+LexiPugh@users.noreply.github.com>
2024-11-05 22:32:13 -0500

    Add Day 300

--- coding-lang-calculation-file/code.sql
+++ coding-lang-calculation-file/code.sql
@@ -2440,3 +2440,16 @@ FROM
 ORDER BY
 	r.surname,
 	r.firstname
+
+
+SELECT
+	m.firstname AS memfname,
+	m.surname AS memsname,
+	r.firstname AS recfname,
+	r.surname AS recsname
+FROM
+	cd.members AS m LEFT JOIN cd.members AS r
+	ON m.recommendedby = r.memid
+ORDER BY
+	memsname,
+	memfname
